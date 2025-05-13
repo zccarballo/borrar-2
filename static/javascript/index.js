7482b5b119aae6fde68b40ed4e04211faa65b205
zccarballo <zchacar.genius@gmail.com>
2025-05-13 10:48:10 +0200

    carpetas actualizadas

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -43,3 +43,68 @@ for (let contador = 0; contador < arrayDatos.length; contador++) {
     "ten este valor: " + arrayDatos[contador]
   );
 }
+
+/*utilizo el + para concatenar varias cosas dentro de console.log;
+ *también podría dejar solo una string y utilizar $ para "llamar" a cada una de mis variables, pero en este caso no funciona,
+ *seguramente por ser un bucle for*/
+
+//EXER4: creacion de etiquetas para mostrar os datos en HTML
+
+//IMPRIMO O ARRAY
+let arrayParaHTML = document.createElement("div");
+arrayParaHTML.className = "estilo-div";
+arrayParaHTML.innerHTML = arrayDatos;
+document.body.append(arrayParaHTML);
+console.log("Este e o div que inclue o meu array", arrayParaHTML);
+
+//IMPRIMO O OBXECTO - desta forma non podo imprimir o obxecto, pero si cada dato do obxecto
+let obxectoParaHTML = document.createElement("div");
+obxectoParaHTML.className = "estilo-div2";
+obxectoParaHTML.innerHTML = obxectoDatos.primerDato; //asi accedo ao primeiro dato de obxectoDatos
+document.body.append(obxectoParaHTML);
+console.log("Este e o div que inclue o meu obxecto", obxectoParaHTML);
+
+//(for .. in) con este bucle especial podo acceder a informacion do obxecto; (for...of) seria o equivalente para array
+
+for (const clave in obxectoDatos) {
+  console.log("clave ", clave, "valor do obxeto ", obxectoDatos[clave]);
+}
+//con este bucle xa podo imprimir o obxecto
+
+//desta forma creo un div por cada dato:
+
+for (const clave in obxectoDatos) {
+  console.log("clave ", clave, "valor do obxeto ", obxectoDatos[clave]);
+  let obxectoParaHTML = document.createElement("div");
+  obxectoParaHTML.className = "estilo-div";
+  obxectoParaHTML.innerHTML = obxectoDatos[clave];
+  document.body.append(obxectoParaHTML);
+  console.log("Este e o div que inclue o meu obxecto", obxectoParaHTML);
+}
+
+//desta forma creo un div no que meto todos os datos:
+
+// let contedorObxecto = document.createElement("div");
+// contedorObxecto.className = "estilo-div2";
+// document.body.append(contedorObxecto);
+// for (const clave in obxectoDatos) {
+//   obxectoParaHTML.innerHTML = obxectoDatos[clave];
+//   contedorObxecto.append(obxectoDatos[clave]);
+//   console.log("Este e o div que inclue o meu obxecto", obxectoParaHTML);
+// }
+
+//IMPRIMO A ITERACIÓN
+
+for (let contador = 0; contador < arrayDatos.length; contador++) {
+  let bucle =
+    "o elemento do array en posicion: " +
+    contador +
+    " ten este valor: " +
+    arrayDatos[contador]; //Creo unha variable coa accion que se vai repetir en bucle
+  console.log(bucle);
+  let bucleParaHTML = document.createElement("div");
+  bucleParaHTML.className = "estilo-div2";
+  bucleParaHTML.innerHTML = bucle; //Dentro do contido do meu div introduzo a variable que creei para o bucle
+  document.body.append(bucleParaHTML);
+  console.log("Este e o div que inclue o meu bucle", bucleParaHTML);
+}
